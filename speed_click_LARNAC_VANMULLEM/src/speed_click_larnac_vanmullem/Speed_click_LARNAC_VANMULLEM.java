84a34420cc5d72724989d64883428a9a223bfc85
Solene_Van_Mullem <solene.vml@gmail.com>
2022-12-06 11:08:22 +0100

    création fenetre jframe et jlabel fait début Jpanel

--- speed_click_LARNAC_VANMULLEM/src/speed_click_larnac_vanmullem/Speed_click_LARNAC_VANMULLEM.java
+++ speed_click_LARNAC_VANMULLEM/src/speed_click_larnac_vanmullem/Speed_click_LARNAC_VANMULLEM.java
@@ -4,8 +4,10 @@
  */
 package speed_click_larnac_vanmullem;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -20,19 +22,22 @@ public class Speed_click_LARNAC_VANMULLEM {
         
         //création JFrame de la fenetre avec bordure et titre
         JFrame frame = new JFrame("Speed Click Game");
-        frame.setPreferredSize(new Dimension(500,500)); // taille de la fenetre
+        frame.setPreferredSize(new Dimension(800,600)); // taille de la fenetre
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //ferme la fenetre = prog s'arrete
         
-        frame.setLayout(new FlowLayout()); //place les deux labels cote à cote
+        //frame.setLayout(new GridLayout(1,2)); grille de 1 ligne et 2 colonnes --> 1 label = 1 case
+        frame.setLayout(new BorderLayout()); //version de GridLayout mais en mieux
         
-        JLabel label1 = new JLabel ();
-        frame.add(label1);
-        label1.setBorder(BorderFactory.createTitledBorder("Bienvenue dans le jeu !"));
+        JLabel label1 = new JLabel ();    
+        label1.setBorder(BorderFactory.createTitledBorder("Bienvenue dans le jeu !"));//titre du label
+        frame.add(label1,BorderLayout.CENTER); //zone principale (=zone de jeu
         
-        JLabel label2 = new JLabel ();
-        frame.add(label2);
-        label2.setBorder(BorderFactory.createTitledBorder("Etes vous pret à jouer ?"));
-        
+        JLabel label2 = new JLabel ("Choisissez votre chrono et augmentez votre score en cliquant le plus vite possible sur les boutons rouges.");
+        label2.setBorder(BorderFactory.createTitledBorder("Règles du jeu :")); //titre
+        frame.add(label2, BorderLayout.NORTH);
+        // un seul label par zone
+        // si plusieurs label --> Jpanel
+       
         frame.pack(); //calcul sa taille final
         frame.setVisible(true); //montre la fenetre
         
