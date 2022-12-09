7b0e3b2d770ae66b08890d0055918f32ceed2b35
anaelle larnac <anaelle.larnac@epfedu.fr>
2022-12-09 11:16:30 +0100

    avancée classe cellule de grille +plateau de jeu sur le meme modele que le puissance 4

--- speed_click_LARNAC_VANMULLEM/src/speed_click_larnac_vanmullem/bouton.java
+++ speed_click_LARNAC_VANMULLEM/src/speed_click_larnac_vanmullem/bouton.java
@@ -8,11 +8,6 @@ package speed_click_larnac_vanmullem;
  *
  * @author Anaëlle
  */
-public class celluledegrille {
-    bouton boutonCourant;
+public class bouton {
     
-    public void affecterbouton(bouton jetonCourant) { //permet de prendre un 
-        //nouveau jeton et de l'ajouter à ceux déjà existant
-        this.boutonCourant = jetonCourant;
-    }
 }
--- speed_click_LARNAC_VANMULLEM/src/speed_click_larnac_vanmullem/plateaudejeu.java
+++ speed_click_LARNAC_VANMULLEM/src/speed_click_larnac_vanmullem/plateaudejeu.java
@@ -9,5 +9,26 @@ package speed_click_larnac_vanmullem;
  * @author Anaëlle
  */
 public class plateaudejeu {
+    celluledegrille[][] grille = new celluledegrille [6][7];
     
+    public plateaudejeu() {//constructeur de la classe
+     for(int i=0;i<6;i++){
+            for(int j=0;j<7;j++){
+                grille [i][j]=new celluledegrille();//initialise un tableau de 42 cases
+            }
+        }  
+    }
+    
+    public int ajouterJetonDanstableau(bouton j, int c){//
+        for(int i=0;i<7;i++){//on balaie les lignes
+            for(int d=0;d<7;d++){
+                
+            }
+            
+                return i;
+            }
+            
+        
+   return -1;   //on rajoute pour ne pas créer un bug  
+    }
 }
