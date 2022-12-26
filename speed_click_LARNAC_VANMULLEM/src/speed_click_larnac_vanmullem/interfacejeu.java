182c67e3116b11457801bff354466901b60de40f
anaelle larnac <anaelle.larnac@epfedu.fr>
2022-12-27 00:03:24 +0100

    copier-coller du mail pour récupérer le fichier de l'interface

--- speed_click_LARNAC_VANMULLEM/src/speed_click_larnac_vanmullem/interfacejeu.java
+++ speed_click_LARNAC_VANMULLEM/src/speed_click_larnac_vanmullem/interfacejeu.java
@@ -4,6 +4,15 @@
  */
 package speed_click_larnac_vanmullem;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 /**
  *
  * @author Anaëlle
@@ -12,6 +21,39 @@ public class interfacejeu {
         public static void main(String[] args) {
         interfacejeu interfacejeu = new interfacejeu();//on creer un jeu 
        
+    public void affichage(){
+    //création JFrame de la fenetre avec bordure et titre
+        JFrame frame = new JFrame("Speed Click Game");
+        frame.setPreferredSize(new Dimension(800,600)); // taille de la fenetre
+        frame.getContentPane().setBackground(Color.PINK);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //ferme la fenetre = prog s'arrete
+        
+        //frame.setLayout(new GridLayout(1,2)); grille de 1 ligne et 2 colonnes --> 1 label = 1 case
+        frame.setLayout(new BorderLayout()); //version de GridLayout mais en mieux
+        
+        //JLabel label1 = new JLabel ();    
+        //label1.setBorder(BorderFactory.createTitledBorder("Bienvenue dans le jeu !"));//titre du label
+        //frame.add(label1,BorderLayout.CENTER); //zone principale (=zone de jeu
+        
+        JLabel label2 = new JLabel ("Choisissez votre chrono et augmentez votre score en cliquant le plus vite possible sur les boutons rouges.");
+        label2.setBorder(BorderFactory.createTitledBorder("Règles du jeu :")); //titre
+        frame.add(label2, BorderLayout.NORTH);
+        // un seul label par zone
+        // si plusieurs label --> Jpanel
+        
+        
+        JPanel casePrincipale = new JPanel(new GridLayout(3,3));
+        casePrincipale.setBorder(BorderFactory.createTitledBorder("Bienvenue dans le jeu !"));
+        frame.add(casePrincipale, BorderLayout.CENTER);
+            
+        JPanel caseSecondaire = new JPanel(new GridLayout(1,2));//on fait une grille avec les éléments
+        caseSecondaire.setBorder(BorderFactory.createTitledBorder("Score :"));
+        frame.add(caseSecondaire, BorderLayout.SOUTH);
+        
+        frame.pack(); //calcul sa taille final
+        frame.setVisible(true); //montre la fenetre
+
+     
+ }
     }
 
-}
