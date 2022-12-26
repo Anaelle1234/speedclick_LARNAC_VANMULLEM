13b631dbebdd42602e36b4df5d6d711895b6eabc
anaelle larnac <anaelle.larnac@epfedu.fr>
2022-12-26 23:48:48 +0100

    - ajout de quelques commentaires dans console - création de la classe interfacejeu pour gérer l'interface : créer d'un jeu à partir de l'interface

---
+++ speed_click_LARNAC_VANMULLEM/src/speed_click_larnac_vanmullem/interfacejeu.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package speed_click_larnac_vanmullem;
+
+/**
+ *
+ * @author Anaëlle
+ */
+public class interfacejeu {
+        public static void main(String[] args) {
+        interfacejeu interfacejeu = new interfacejeu();//on creer un jeu 
+       
+    }
+
+}
